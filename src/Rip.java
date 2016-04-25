f5b8bf897c831202e3c2b90a6e01af7e2f763f89
Yeray <ylagef@gmail.com>
2016-04-25 16:49:18 +0200

    Falta comprobar eth0 para saltar error.

---
+++ src/Rip.java
@@ -0,0 +1,55 @@
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.*;
+
+public class Rip {
+    static String iplocal = null;
+    static String puertolocal = "5512";
+
+    public static void main(String[] args) throws UnknownHostException, SocketException {
+
+        configIP(args); //Lee y asigna la ip inicial dependiendo de argumentos o local
+
+        System.out.println("La IP y puerto locales son: " + iplocal + ":" + puertolocal);
+
+        ServerSocket sskt;
+
+        try {
+            sskt = new ServerSocket(Integer.parseInt(puertolocal));
+            System.out.println("Escuchando en el puerto: " + puertolocal);
+            while (true) {
+                Socket cskt = sskt.accept(); //Creamos el socket de conexion con el cliente
+                System.out.print("Atendiendo al cliente: " + cskt.getInetAddress());
+                OutputStream os = cskt.getOutputStream();
+                DataOutputStream flujo = new DataOutputStream(os);
+                flujo.writeUTF("Hola cliente, soy el servidor del host: " + iplocal);
+                cskt.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void configIP(String[] args) throws UnknownHostException, SocketException {
+
+        if (args.length != 0) {
+            String[] entrada = args[0].split(":");
+            iplocal = entrada[0];
+            if (entrada.length != 1) {
+                puertolocal = entrada[1];
+            }
+        } else {
+
+            iplocal = Inet4Address.getLocalHost().getHostAddress();
+
+            //FALTA COMPROBAR QUE SEA ETH0
+
+        }
+    }
+
+}
