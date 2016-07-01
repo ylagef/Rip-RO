0eb12d3ebe740dd3d6359dce2317f0bcb0a5ddb6
Yeray <ylagef@gmail.com>
2016-07-01 12:37:08 +0200

    Subredes comunes en proceso.

--- src/ProcesadorPaquetes.java
+++ src/ProcesadorPaquetes.java
@@ -81,20 +81,28 @@ public class ProcesadorPaquetes implements Runnable {
             }
 
             if (tabla.containsKey(encaminamientoNuevo.getDireccionInet().getHostAddress())) { //Ya tengo esta subred
-
+                boolean stop = false;
                 Iterator it = tabla.entrySet().iterator();
                 while (it.hasNext()) {
                     Map.Entry e = (Map.Entry) it.next();
-                    if (mismaSubred(encaminamientoNuevo, (Encaminamiento) e.getValue())) {
-
+                    if (!encaminamientoNuevo.getDireccionInet().getHostAddress().contentEquals(((Encaminamiento) e.getValue()).getDireccionInet().getHostAddress())) {
+                        if (mismaSubred(encaminamientoNuevo, (Encaminamiento) e.getValue())) {
+                            System.out.println("1 - No añade.");
+                            stop = true;
+                        } else {
+                            System.out.println("2 - Añade.");
+                        }
                     }
+                }
+
+                if (stop) {
+                    continue;
                 }
 
                 Encaminamiento encaminamientoActual =
                         tabla.get(encaminamientoNuevo.getDireccionInet().getHostAddress()); //El de la tabla actual
 
                 if (encaminamientoNuevo.getDireccionInet().getHostAddress().contains(receptor.getIpLocal().getHostAddress())) {
-                    System.out.println("SOY YO");
                     continue;
                 }
 
@@ -134,20 +142,32 @@ public class ProcesadorPaquetes implements Runnable {
         }
     }
 
-    InetAddress mismaSubred(Encaminamiento e1, Encaminamiento e2) throws UnknownHostException {
-        int mascara = 0xffffffff << (32 - Integer.valueOf(e.getMascaraInt()));
+    boolean mismaSubred(Encaminamiento e1, Encaminamiento e2) throws UnknownHostException {
+        InetAddress net1 = netFromEnc(e1.getDireccionInet(), e1.getMascaraInt()); //net del e1
+        InetAddress net2 = netFromEnc(net1, e2.getMascaraInt()); //net del net1 con la mascara del 2
+
+        if (net1.getHostAddress().replaceAll("/", "").contentEquals(net2.getHostAddress().replaceAll("/", ""))) {
+            System.out.println("Misma subred");
+            return true;
+        } else {
+            System.out.println("Distinta subred");
+            return false;
+        }
+
+    }
+
+    InetAddress netFromEnc(InetAddress dir, int masc) throws UnknownHostException {
+        int mascara = 0xffffffff << (32 - Integer.valueOf(masc));
         byte[] mascaraBytes = new byte[]{
                 (byte) (mascara >>> 24), (byte) (mascara >> 16 & 0xff), (byte) (mascara >> 8 & 0xff), (byte) (mascara & 0xff)};
 
-        byte[] ip = e.getDireccionInet().getAddress();
+        byte[] ip = dir.getAddress();
 
         int aux1 = ip[0] & mascaraBytes[0];
         int aux2 = ip[1] & mascaraBytes[1];
         int aux3 = ip[2] & mascaraBytes[2];
         int aux4 = ip[3] & mascaraBytes[3];
 
-        InetAddress network = InetAddress.getByAddress(new byte[]{(byte) aux1, (byte) aux2, (byte) aux3, (byte) aux4});
-
+        return InetAddress.getByAddress(new byte[]{(byte) aux1, (byte) aux2, (byte) aux3, (byte) aux4});
     }
-
 }
